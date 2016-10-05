348769bbd5baf9d6d9f071dc3858843d89de383d
Pradeep Nayak <pradpnayak@gmail.com>
2016-10-05 18:46:33 +0530

    code cleanup

--- sql/refund_install.sql
+++ sql/refund_install.sql
@@ -7,14 +7,6 @@ INSERT INTO
 VALUES
    (@option_group_id_account_relationship, 'Accounts Payable Account is', @option_group_id_account_relationship_val+1, 'Accounts Payable Account is', NULL, 0, NULL, @option_group_id_account_relationship_wt+1, 'Accounts Payable Account is', 0, 0, 1, @contributeCompId, NULL);
 
-SELECT @option_group_id_account_relationship_val  := MAX(CAST(value as UNSIGNED)) FROM civicrm_option_value WHERE option_group_id = @option_group_id_account_relationship;
-SELECT @financial_type_id := id FROM civicrm_financial_type WHERE name = 'Donation';
-SELECT @financial_account_id := id FROM civicrm_financial_account WHERE name = 'Accounts Payable';
-
-INSERT INTO 
-   civicrm_entity_financial_account (entity_table, entity_id, account_relationship, financial_account_id)
-VALUES
-   ('civicrm_financial_type', @financial_type_id, @option_group_id_account_relationship_val, @financial_account_id);
 
 SELECT @option_group_id_contrib_status := max(id) from civicrm_option_group where name = 'contribution_status';
 SELECT @contributeCompId := max(id) FROM civicrm_component where name = 'CiviContribute';
